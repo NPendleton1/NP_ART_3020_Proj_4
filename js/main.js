de73ab7cfdaaf2f00875743a2cd4e4ce47279dbe
NPendleton1 <nicholas.pendleton10@gmail.com>
2025-04-19 03:02:42 -0500

    Yerp

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// JS scripts placed here
-var tl = gsap.timeline({repeat: 2, repeatDelay: 1});
-tl.to("#el", {x: 100, duration: 1});
-tl.to("#el", {rotation: 50, duration: 1});
-tl.to("#el", {backgroundColor: "red"});
